726272b98edfcbe022ac609ce829b928a8899434
Nitros64 <nitros.n64@gmail.com>
2025-01-09 19:56:28 +0100

    test only 2

--- src/test/java/com/example/tarea1/service/ServiceTest2.java
+++ src/test/java/com/example/tarea1/service/ServiceTest2.java
@@ -3,9 +3,19 @@ package com.example.tarea1.service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 public class ServiceTest2 {
     @Test
     void contextLoads() {
+
+
+    }
+
+    @Test
+    void testServiceTestOnly() {
+        String result = "Hello World2";
+        assertEquals("Hello World", result, "getTask1() should return 'Hello World'");
     }
 }
